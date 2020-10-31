76da31ca4663f0e567a8cfcd12eae49427f89984
Daisuke Morita <daisuke.marubinotto@gmail.com>
2020-10-31 12:05:01 +0900

    concept: add Program.onUrlChange

--- concept/src/main/scala/jp/sysart/fpui/FunctionalUI.scala
+++ concept/src/main/scala/jp/sysart/fpui/FunctionalUI.scala
@@ -5,6 +5,8 @@ import org.scalajs.dom.Element
 import slinky.core.facade.ReactElement
 import slinky.web.ReactDOM
 import org.scalajs.dom.experimental.URL
+import org.scalajs.dom.raw.CustomEvent
+import org.scalajs.dom.raw.Event
 
 object FunctionalUI {
   type Effect[Msg] = (Msg => Unit) => Unit
@@ -12,11 +14,12 @@ object FunctionalUI {
   case class Program[Model, Msg](
       init: (URL) => (Model, Effect[Msg]),
       view: (Model, Msg => Unit) => ReactElement,
-      update: (Msg, Model) => (Model, Effect[Msg])
+      update: (Msg, Model) => (Model, Effect[Msg]),
+      onUrlChange: Option[URL => Msg] = None
   )
 
   class Runtime[Model, Msg](container: Element, program: Program[Model, Msg]) {
-    private val init = program.init(new URL(dom.document.location.href))
+    private val init = program.init(new URL(dom.window.location.href))
     private var state = init._1
 
     def dispatch(msg: Msg): Unit = {
@@ -30,8 +33,22 @@ object FunctionalUI {
       effect(dispatch)
     }
 
+    program.onUrlChange.map(onUrlChange => {
+      val listener =
+        (e: Event) => dispatch(onUrlChange(new URL(dom.window.location.href)))
+      dom.window.addEventListener("popstate", listener)
+      dom.window.addEventListener("pushstate", listener)
+    })
+
     apply(init)
   }
 
   def noEffect[Msg]() = (dispatch: Msg => Unit) => ()
+
+  def pushUrl(url: String) = {
+    // Calling history.pushState() or history.replaceState() won't trigger any event by default.
+    // https://developer.mozilla.org/en-US/docs/Web/API/WindowEventHandlers/onpopstate
+    dom.window.history.pushState((), "", url)
+    dom.window.dispatchEvent(new CustomEvent("pushstate", ()))
+  }
 }
